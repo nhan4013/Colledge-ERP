679450d40812d80f6c3226fe8cb3f0ab3a2280fb
nhanpham4013 <nhanpham4013@gmail.com>
2025-08-24 18:46:41 +0700

    refactor: change path of static file

--- colledge_system/urls.py
+++ colledge_system/urls.py
@@ -20,5 +20,8 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include('main_app.urls')),
+    path("", include('main_app.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
--- main_app/admin.py
+++ main_app/admin.py
@@ -6,10 +6,26 @@ from .models import *
 
 
 class UserModel(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'user_type', 'gender')
+    list_filter = ('user_type', 'gender')
+    search_fields = ('email',)
     ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('user_type', 'gender', 'profile_pic', 'address', 'fcm_token')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'user_type', 'gender', 'profile_pic', 'address', 'fcm_token', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}
+        ),
+    )
 
 
-admin.site.register(CustomUser, UserModel)
+admin.site.register(CustomUser,UserModel)
 admin.site.register(Staff)
 admin.site.register(Student)
 admin.site.register(Course)
@@ -18,4 +34,3 @@ admin.site.register(IssuedBook)
 admin.site.register(Library)
 admin.site.register(Subject)
 admin.site.register(Session)
-admin.site.register(CustomUser)
