98fb820dee3b3113de18857e8a655fa9c77d3745
nhanpham4013 <nhanpham4013@gmail.com>
2025-08-24 17:51:02 +0700

    Add login page template, implement result editing functionality, and enhance admin model registration

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-admin.site.register(CustomUser)
+from .models import *
 
+
+class UserModel(UserAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, UserModel)
+admin.site.register(Staff)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Book)
+admin.site.register(IssuedBook)
+admin.site.register(Library)
+admin.site.register(Subject)
+admin.site.register(Session)
+admin.site.register(CustomUser)
