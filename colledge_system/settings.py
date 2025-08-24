027ac28f5f0624d2f950103803fa94e5545e52db
nhanpham4013 <nhanpham4013@gmail.com>
2025-08-24 17:57:01 +0700

    Add middleware for user role-based redirection and implement HOD, Staff, and Student views
    - Added `LoginCheckMiddleWare` to handle user authentication and redirect based on user type.
    - Updated `settings.py` to include the new middleware.
    - Implemented HOD, Staff, and Student views in `hod_views.py`, `staff_views.py`, and `student_views.py` respectively.
    - Enhanced attendance management features for staff and students.
    - Added functionality for managing courses, subjects, and staff/student profiles.
    - Implemented feedback and leave application features for both staff and students.

--- colledge_system/settings.py
+++ colledge_system/settings.py
@@ -55,7 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'main_app.middleware.LoginCheckMiddleWare'
 ]
 
 AUTHENTICATION_BACKENDS = [
